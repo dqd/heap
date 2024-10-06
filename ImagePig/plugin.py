1843d5003b0c2b058444c6265c36374c01b4a0b3
Pavel Mises <id@dqd.cz>
2024-10-07 00:28:40 +0200

    Faceswap

--- ImagePig/plugin.py
+++ ImagePig/plugin.py
@@ -47,5 +47,38 @@ class ImagePig(callbacks.Plugin):
         image, [getopts({"model": ("somethingWithoutSpaces", "model")}), "text"]
     )
 
+    def faceswap(self, irc, msg, args, source, target):
+        """<source_url> <target_url>
+
+        Performs a faceswap using a face from <source_url> which replaces face in <target_url>.
+        """
+        api_key = self.registryValue("api_key")
+        storage_days = self.registryValue("storage_days")
+
+        try:
+            r = requests.post(
+                "https://api.imagepig.com/faceswap",
+                headers={"Api-Key": api_key},
+                json={
+                    "source_image_url": source,
+                    "target_image_url": target,
+                    "storage_days": storage_days,
+                },
+            )
+
+            if r.ok:
+                url = r.json().get("image_url")
+            else:
+                r.raise_for_status()
+        except Exception as e:
+            irc.error(unicode(e), Raise=True)
+
+        if url:
+            irc.reply(url)
+        else:
+            irc.error("No image was returned for the faceswap.")
+
+    faceswap = wrap(faceswap, ["somethingWithoutSpaces", "somethingWithoutSpaces"])
+
 
 Class = ImagePig
